c5caadafdde7aaae6f9348d017adac74f22873a4
Matthew Sherman <msherman0801@gmail.com>
2018-09-12 00:30:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.splice(3,0,name);
+}
+function destructivelyPrependKitten(name) {
+  kittens.splice(0,0,name);
+}
+function destructivelyRemoveLastKitten() {
+  kittens.splice(2,1);
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.splice(0,1);
+}

a3e22f6a1994fc0fb646a9dda9528ff610be18a9
Matthew Sherman <msherman0801@gmail.com>
2018-09-12 00:56:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function appendKitten(name) {
   return test
 }
 function prependKitten(name) {
-  var test = kittens.concat(name,...kittens)
-  return test;
+  var test = kittens
+  return test.push(name);
 }
